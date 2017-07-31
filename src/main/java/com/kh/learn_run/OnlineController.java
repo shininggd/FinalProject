5b074e67ca60c0d336eaf96271799fdaac9dd4c2
HanJin <ups362@gmail.com>
2017-07-31 17:29:10 +0900

    ㅇㅇㅇㅇ

--- src/main/java/com/kh/learn_run/OnlineController.java
+++ src/main/java/com/kh/learn_run/OnlineController.java
@@ -9,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OnlineController {
 
 	@RequestMapping(value="onlineStudyPage")
-	public void goOnlineStudy(String room_id,Model model) {
+	public String goOnlineStudy(String room_id,Model model,String category) {
 		System.out.println("room="+room_id);
 		model.addAttribute("room_id", room_id);
+		
+		String path = null;
+		if(category.equals("영여회화")) {
+			path = "study/online/subject/onlineStudyEnglish";
+		}
+		
+		return path;
 	}
 }
